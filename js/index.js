e70901a7bc14691f46f8a92b8f3fa1f89ce2d1ae
Bryan Portillo <bportillo701@gmail.com>
2023-06-08 02:49:48 -0600

    adding more design to card

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ let date = document.getElementById("date");
 let inputLocation = document.getElementById("inputLocation");
 let temperature = document.getElementById("temperature");
 let search = document.getElementById("search");
+let imgWeather = document.getElementById("imgWeather");
 function convertDate(fecha) {
     const date = new Date(fecha);
     const dia = date.getDate().toString().padStart(2, '0');
@@ -20,17 +21,22 @@ function convertDate(fecha) {
 function setWeatherData(weatherResponse) {
     condition.innerHTML = weatherResponse.current.condition.text;
     date.innerHTML = convertDate(weatherResponse.location.localtime);
-    // inputLocation.value = weatherResponse.location.name;
     temperature.innerHTML = Math.floor(weatherResponse.current.temp_c).toString();
+    imgWeather.src = weatherResponse.current.condition.icon.replace("64x64", "128x128");
+    imgWeather.alt = weatherResponse.current.condition.text;
 }
-// ( async () => {
+async function makeSearchAndSetFields(search) {
+    let weatherResponse = await weatherApiService.getRealTimeWeather(search);
+    setWeatherData(weatherResponse);
+}
+// (async () => {
 // 	let { latitude, longitude } = await geolocationService.getUserLocation();
-// 	let weatherResponse = await weatherApiService.getRealTimeWeather(`${latitude}, ${longitude}`);
-// 	setWeatherData(weatherResponse);
+// 	makeSearchAndSetFields(`${latitude}, ${longitude}`)
 // })();
 search.addEventListener('click', async (e) => {
     e.preventDefault();
     let cleanInputData = inputLocation.value.trim();
-    let weatherResponse = await weatherApiService.getRealTimeWeather(cleanInputData);
-    setWeatherData(weatherResponse);
+    if (cleanInputData) {
+        makeSearchAndSetFields(cleanInputData);
+    }
 });
--- ts/index.ts
+++ ts/index.ts
@@ -12,6 +12,7 @@ let date = document.getElementById("date") as HTMLElement;
 let inputLocation = document.getElementById("inputLocation") as HTMLInputElement;
 let temperature = document.getElementById("temperature") as HTMLElement;
 let search = document.getElementById("search") as HTMLElement;
+let imgWeather = document.getElementById("imgWeather") as HTMLImageElement;
 
 function convertDate(fecha: string): string {
 	const date = new Date(fecha);
@@ -27,6 +28,8 @@ function setWeatherData(weatherResponse: IRealTimeWeather) {
 	condition.innerHTML = weatherResponse.current.condition.text;
 	date.innerHTML = convertDate(weatherResponse.location.localtime);
 	temperature.innerHTML = Math.floor(weatherResponse.current.temp_c).toString();
+	imgWeather.src = weatherResponse.current.condition.icon.replace("64x64","128x128");
+	imgWeather.alt = weatherResponse.current.condition.text;
 }
 
 async function makeSearchAndSetFields(search: string) {
@@ -34,15 +37,17 @@ async function makeSearchAndSetFields(search: string) {
 	setWeatherData(weatherResponse);
 }
 
-(async () => {
-	let { latitude, longitude } = await geolocationService.getUserLocation();
-	makeSearchAndSetFields(`${latitude}, ${longitude}`)
-})();
+// (async () => {
+// 	let { latitude, longitude } = await geolocationService.getUserLocation();
+// 	makeSearchAndSetFields(`${latitude}, ${longitude}`)
+// })();
 
 search.addEventListener('click', async (e) => {
 	e.preventDefault();
 	let cleanInputData = inputLocation.value.trim();
-	makeSearchAndSetFields(cleanInputData);
+	if (cleanInputData){
+		makeSearchAndSetFields(cleanInputData);
+	}
 })
 
 
