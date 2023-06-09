1ab221e8da715e29c7b8ac14b1cfbbecf925555f
Bryan Portillo <bportillo701@gmail.com>
2023-06-08 18:00:43 -0600

    fixing days of the week array

--- ts/index.ts
+++ ts/index.ts
@@ -117,15 +117,15 @@ function getDayName(forecastDate: string) {
 	let todayDate = moment();
 	let todayDay = todayDate.day();
 
-	let days = {
-		1: "Monday",
-		2: "Tuesday",
-		3: "Wednesday",
-		4: "Thursday",
-		5: "Friday",
-		6: "Saturday",
-		7: "Sunday"
-	};
+	let days = [
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday"
+	];
 
 	return todayDay === dayForecast ? "Today" : days[dayForecast];
 }
